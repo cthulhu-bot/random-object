63b57e60636d28e1b2066474fdb48fe9410fe475
Joshua Stupplebeen <jpstup@gmail.com>
2017-04-05 14:25:01 -0600

    working in single depth case, but fails for two

--- src/core.js
+++ src/core.js
@@ -1,17 +1,15 @@
 const objDepth = (obj, acc = 0) => {
-    console.log('acc: ', acc);
-    if (Object.keys(obj).length === 0) {
+    // Filter object by keys containing child objects
+    const objectValues = Object.keys(obj).filter(key => { return typeof obj[key] === 'object' })
+    // if no child objects are found return the current object depth
+    if (objectValues.length === 0)
 	return acc;
-    }
 
-    const foo = Object.keys(obj)
-    .reduce((depths, objKey) => {
-	    if (!!obj[objKey]) {
-		depths = depths.concat([objDepth(obj.objKey, acc++)]);
-	    }
-	    console.log('depths: ', depths);
-	    return depths;
-	}, []);
+    // else recursively call objDepth increasing the current object depth
+    // compile these depths into an array of each key's final discovered child object depth
+    // finally return the maximum depth discovered
+    return objectValues.reduce((keyDepths,key) => { return keyDepths.concat(objDepth(obj[key], ++acc))}, [])
+                       .reduce((a,b) => (Math.max(a,b)))
 };
 
 const objWidth = (obj) => {
--- test/test.js
+++ test/test.js
@@ -4,14 +4,18 @@ test('empty object should have a width of 0', () => {
     expect(objWidth({})).toBe(0);
 });
 
-//test('empty object should have a length of 0', () => {
-//    expect(objDepth({})).toBe(0);
-//});
-
-test('object containing no other objects should return 1', () => {
-    expect(objDepth({a : 'a'})).toBe(1);
+test('empty object should have a depth of 0', () => {
+    expect(objDepth({})).toBe(0);
 });
 
-//test('object contain a single object should return 2', () => {
-//    expect(objDepth({a : {b : 'b' }})).toBe(2)
-//});
+test('object containing no other object values should return a depth of 0', () => {
+	expect(objDepth({a: 'a'})).toBe(0);
+});
+
+test('object contain a single object should return a depth of 1', () => {
+    expect(objDepth({a: {b: 'b' }})).toBe(1)
+});
+
+test('object containing two object should still return a depth of 1', () => {
+    expect(objDepth({a: {b: 'b' }, c: {d: 'd'}})).toBe(1)
+});
