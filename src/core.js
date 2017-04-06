cec70c6769dffcabeb3bddc65e28188131d1c9aa
Joshua Stupplebeen <jpstup@gmail.com>
2017-04-05 19:40:21 -0600

    depth calculator working

--- src/core.js
+++ src/core.js
@@ -8,7 +8,10 @@ const objDepth = (obj, acc = 0) => {
     // else recursively call objDepth increasing the current object depth
     // compile these depths into an array of each key's final discovered child object depth
     // finally return the maximum depth discovered
-    return objectValues.reduce((keyDepths,key) => { return keyDepths.concat(objDepth(obj[key], ++acc))}, [])
+    return objectValues.reduce((keyDepths,key) => { 
+	                 const ret = keyDepths.concat(objDepth(obj[key], ++acc))
+                         acc = 0
+                         return ret }, [])
                        .reduce((a,b) => (Math.max(a,b)))
 };
 
--- test/test.js
+++ test/test.js
@@ -12,10 +12,18 @@ test('object containing no other object values should return a depth of 0', () =
 	expect(objDepth({a: 'a'})).toBe(0);
 });
 
-test('object contain a single object should return a depth of 1', () => {
+test('object containing a single object should return a depth of 1', () => {
     expect(objDepth({a: {b: 'b' }})).toBe(1)
 });
 
-test('object containing two object should still return a depth of 1', () => {
+test('object containing two surface level objects should still return a depth of 1', () => {
     expect(objDepth({a: {b: 'b' }, c: {d: 'd'}})).toBe(1)
+});
+
+test('object containing two nested objects should return a depth of 2', () => {
+    expect(objDepth({a: {b: {c: 'c'}}})).toBe(2)
+});
+
+test('object containing three nested objects should return a depth of 3', () => {
+    expect(objDepth({a: {b: {c: {d: 'd'}}}})).toBe(3)
 });
