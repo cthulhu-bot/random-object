187698632194ed1b61ef643528e41789c4db37f9
Joshua Stupplebeen <jpstup@gmail.com>
2017-04-06 23:50:29 -0600

    working on object testing

--- src/RandomObject.js
+++ src/RandomObject.js
@@ -4,11 +4,11 @@ import {
     random1To10 
 } from './util';
 
-const randomObject = (length = util.random1To10(), depth = util.random1To10()) => {
-    for (let i = 0; i < length; i++) {
-	console.log(i)
+const randomObject = (width = util.random1To10(), depth = util.random1To10()) => {
+    for (let i = 0; i < width; i++) {
     }
-};
+    return {a: {b: {c: 'c'}}}
+}
 
 module.exports = {
     randomObject,
--- test/randomObject.spec.js
+++ test/randomObject.spec.js
@@ -1,6 +1,12 @@
 import { randomObject } from '../src/RandomObject'
 import { objDepth, objWidth } from '../src/util'
 
-test('', () => {
-	expect(true).toBe(true);
-    });
+test('requesting an object with given lengths/widths works', () => {
+    const obj1 = randomObject(1, 2)
+    expect(objWidth(obj1)).toBe(1);
+    expect(objDepth(obj1)).toBe(2);
+})
+
+test('requesting an object with no params generates an object with random width and depth less that 10', () => {
+    expect(true).toBe(true);
+})
